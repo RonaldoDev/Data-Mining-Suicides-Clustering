775498a55a5448a4b38a5d21550490647f932657
Daniel Kock <danielk.kock@gmail.com>
2019-06-11 23:43:52 -0300

    adicionado os datasets e testado o agrupamento com cluster hierárquico

--- cluster.py
+++ cluster.py
@@ -1,29 +1,62 @@
-import pandas
 from sklearn.cluster import AgglomerativeClustering
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
-from scipy.cluster.hierarchy import dendrogram, linkage  
+from scipy.cluster.hierarchy import dendrogram
+
+import numpy as np
+
+import pandas
 
 
 FILENAME = 'suicides.csv'
 CLUSTERS = 10
 
 #  lê o csv
-dataFrame = pandas.read_csv(FILENAME)
+dataFrame = pandas.read_csv('csv/' + FILENAME)
+
+# remove as colunas nao usadas
+dataFrame = dataFrame.drop(columns=['ano', 'municipio', 'raca', 'microregiao'])
+
+# print(dataFrame.columns.values)
+
+def plot_dendrogram(model, **kwargs):
+
+    # Children of hierarchical clustering
+    children = model.children_
+
+    # Distances between each pair of children
+    # Since we don't have this information, we can use a uniform one for plotting
+    distance = np.arange(children.shape[0])
+
+    # The number of observations contained in each cluster level
+    no_of_observations = np.arange(2, children.shape[0]+2)
+
+    # Create linkage matrix and then plot the dendrogram
+    linkage_matrix = np.column_stack([children, distance, no_of_observations]).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
 
 # normaliza oss dados de categorico para numerico
-dataFrameNormalized = preprocessing.OrdinalEncoder().fit(dataFrame.values).transform(dataFrame.values)
-# print(dataFrameNormalized)
+data = preprocessing.OrdinalEncoder().fit(dataFrame.values).transform(dataFrame.values)
 
-# faz a clusterização hierarquica
-cluster = AgglomerativeClustering(n_clusters=CLUSTERS, affinity='euclidean', linkage='ward')
-cluster.fit_predict(dataFrameNormalized)
+# # faz a clusterização hierarquica
+model = AgglomerativeClustering(
+    compute_full_tree=True, 
+    linkage="complete", 
+    affinity="euclidean", 
+    n_clusters=CLUSTERS
+)
 
-print(len(cluster.labels_))
+model.fit(data)
 
-# plt.plot(clustering.labels_)
-# plt.ylabel('Fitness')
-# plt.xlabel('Generation')
+print(model.n_leaves_)
+
+dataFrame['cluster'] = model.labels_
+
+print(dataFrame.groupby(dataFrame['cluster'],as_index=False).size())
+
+# plt.title('Hierarchical Clustering Dendrogram')
+# plot_dendrogram(model, labels=model.labels_)
 # plt.show()
-
-plt.scatter(dataFrame.values[:,0],dataFrame.values[:,1], c=cluster.labels_, cmap='rainbow')  
