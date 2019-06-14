aa059245700a531abcf8e5eed7faa900b60b6025
Daniel Kock <danielk.kock@gmail.com>
2019-06-13 21:30:34 -0300

    teta escrito

--- cluster.py
+++ cluster.py
@@ -1,4 +1,5 @@
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import KMeans
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import dendrogram
@@ -14,8 +15,22 @@ CLUSTERS = 10
 #  lê o csv
 dataFrame = pandas.read_csv('csv/' + FILENAME)
 
+dataFrame = dataFrame[
+    (dataFrame['sexo'] != 'NI') & 
+    (dataFrame['estado_civil'] != 'NI') & 
+    (dataFrame['escolaridade'] != 'NI') & 
+    (dataFrame['IDH'] != 'NI') & 
+    (dataFrame['mesoregiao'] != 'NI') &
+    (dataFrame['local_cid'] != 'NI') & 
+    (dataFrame['sexo'] != 'NI') &
+    (dataFrame['Profissao'] != 'NI') &
+    (dataFrame['Faixa_etaria'] != 'NI') &
+    (dataFrame['motivo'] != 'NI') &
+    (dataFrame['local_ocorrencia'] != 'NI') 
+]
+
 # remove as colunas nao usadas
-dataFrame = dataFrame.drop(columns=['ano', 'municipio', 'raca', 'microregiao'])
+dataFrame = dataFrame.drop(columns=['sexo', 'ano', 'municipio', 'raca', 'microregiao'])
 
 # print(dataFrame.columns.values)
 
@@ -38,22 +53,38 @@ def plot_dendrogram(model, **kwargs):
     dendrogram(linkage_matrix, **kwargs)
 
 
+encoder = preprocessing.OrdinalEncoder()
+
 # normaliza oss dados de categorico para numerico
-data = preprocessing.OrdinalEncoder().fit(dataFrame.values).transform(dataFrame.values)
+data = encoder.fit(dataFrame.values).transform(dataFrame.values)
+# data = dataFrame
+
 
 # # faz a clusterização hierarquica
-model = AgglomerativeClustering(
-    compute_full_tree=True, 
-    linkage="complete", 
-    affinity="euclidean", 
-    n_clusters=CLUSTERS
-)
+# model = AgglomerativeClustering(
+#     compute_full_tree=True, 
+#     linkage="complete", 
+#     affinity="euclidean", 
+#     n_clusters=CLUSTERS
+# )
+
+model = KMeans(n_clusters=CLUSTERS)
 
 model.fit(data)
 
-print(model.n_leaves_)
+clusters = model.labels_
+
+centroids = encoder.inverse_transform(model.cluster_centers_)
+
+
+df_centroids = pandas.DataFrame(centroids, columns=dataFrame.columns.values)
+
+print(df_centroids)
 
 dataFrame['cluster'] = model.labels_
+
+# print(dataFrame.values)
+
 
 print(dataFrame.groupby(dataFrame['cluster'],as_index=False).size())
 
