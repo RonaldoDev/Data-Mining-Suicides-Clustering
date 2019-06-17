7bf6ce6a7c93ef816a110024422646205611ac62
Daniel Kock <danielk.kock@gmail.com>
2019-06-16 23:04:17 -0300

    -

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ import pandas
 
 
 FILENAME = 'suicides.csv'
-CLUSTERS = 50
-
+CLUSTERS = 8
 #  lê o csv
 dataFrame = pandas.read_csv('csv/' + FILENAME)
 
@@ -30,7 +29,7 @@ dataFrame = dataFrame[
 ]
 
 # remove as colunas nao usadas
-dataFrame = dataFrame.drop(columns=['sexo', 'ano', 'municipio', 'raca', 'microregiao'])
+dataFrame = dataFrame.drop(columns=['sexo', 'IDH', 'ano', 'local_ocorrencia', 'municipio', 'raca', 'microregiao', 'local_cid'])
 
 # print(dataFrame.columns.values)
 
@@ -38,8 +37,12 @@ dataFrame = dataFrame.drop(columns=['sexo', 'ano', 'municipio', 'raca', 'microre
 encoder = preprocessing.OrdinalEncoder()
 
 # normaliza oss dados de categorico para numerico
-data = encoder.fit(dataFrame.values).transform(dataFrame.values)
+dataNumeric = encoder.fit(dataFrame.values).transform(dataFrame.values)
 # data = dataFrame
+
+scaler = preprocessing.MinMaxScaler()
+
+data = scaler.fit_transform(dataNumeric)
 
 
 # # faz a clusterização hierarquica
@@ -56,10 +59,13 @@ model.fit(data)
 
 clusters = model.labels_
 
-centroids = encoder.inverse_transform(model.cluster_centers_)
+centroidsScaler = scaler.inverse_transform(model.cluster_centers_)
 
+centroids = encoder.inverse_transform(centroidsScaler)
 
 df_centroids = pandas.DataFrame(centroids, columns=dataFrame.columns.values)
+
+df_centroids.drop(columns='motivo')
 
 print(df_centroids)
 
@@ -70,11 +76,11 @@ dataFrame['cluster'] = model.labels_
 
 print(dataFrame.groupby(dataFrame['cluster'],as_index=False).size())
 
-print(dataFrame)
+# print(dataFrame)
 
 print(model.inertia_)
 
-print(model.labels_)
+# print(model.labels_)
 
 # plt.title('Hierarchical Clustering Dendrogram')
 # plot_dendrogram(model, labels=model.labels_)
